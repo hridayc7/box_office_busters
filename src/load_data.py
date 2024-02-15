b8fed191107941d656b0a1c11dbf26c03d7af2e0
Hriday Chhabria <hridaychhabria7@gmail.com>
2024-02-15 11:17:57 -0500

    Refactored experimental code out

--- src/load_data.py
+++ src/load_data.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
 def load_movie_data():
-    # Define the path to the CSV file
-    file_path = '../data/imdb/horror.csv'
-    
-    # Load the data into a pandas DataFrame
-    horror_movies_df = pd.read_csv(file_path)
-    
-    return horror_movies_df
+
 
 if __name__ == '__main__':
     # Load the movie data
